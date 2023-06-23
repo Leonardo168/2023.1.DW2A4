11de46746e38e1ac6af9ca450665ea4c8410fed1
Leonardo168 <leonardo-96@hotmail.com.br>
2023-06-23 20:00:31 -0300

    remoção de arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 const App = () => {
-  return (
-    <h1>Olá Mundo!</h1>
-  )
-}
+  const mensagem = 'Olá Mundo!'
+  
+  return<h1>{mensagem}</h1>
+};
 
 export default App;
