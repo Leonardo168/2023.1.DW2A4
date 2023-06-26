d51b71442a896d98cd9893d3b9b07a0ba971729d
Leonardo168 <leonardo-96@hotmail.com.br>
2023-06-26 16:55:53 -0300

    Ícone e Header

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {CgClose} from "react-icons/cg"
 
 import "./Task.css";
 
@@ -14,7 +15,7 @@ const Task = ({task, handleTaskClick, handleTaskDeletion}) => {
 
             <div className="buttons-container">
                 <button className="remove-task-button" onClick={() => handleTaskDeletion(task.id)}>
-                    X
+                    <CgClose />
                 </button>
             </div>
         </div>
