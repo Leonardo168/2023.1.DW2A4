ef7d63c70fa8c4be29dd273264afa5250f324ae4
Leonardo168 <leonardo-96@hotmail.com.br>
2023-06-26 18:34:15 -0300

    detalhes das tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react"
+import React, {useState} from "react"
 import {v4 as uuidv4} from "uuid";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
+import TaskDetails from "./components/TaskDetails";
 
 import './App.css'
 
@@ -23,7 +25,7 @@ const App = () => {
   
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if (task.id == taskId) return {... task, completed: !task.completed};
+      if (task.id === taskId) return {...task, completed: !task.completed};
 
       return task;
     });
@@ -32,7 +34,7 @@ const App = () => {
   };
 
   const handleTaskAddition = (taskTitle) => {
-    const newTasks = [... tasks, {
+    const newTasks = [...tasks, {
         title: taskTitle,
         id: uuidv4(),
         completed: false,
@@ -43,23 +45,28 @@ const App = () => {
   };
 
   const handleTaskDeletion = (taskId) => {
-    const newTasks = tasks.filter(task => task.id != taskId);
+    const newTasks = tasks.filter(task => task.id !== taskId);
 
     setTasks(newTasks);
   };
 
   return (
-    <>
+    <Router>
       <div className="container">
       <Header />
-        <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks
-          tasks={tasks}
-          handleTaskClick={handleTaskClick}
-          handleTaskDeletion={handleTaskDeletion}
-        />
+        <Route path="/" exact render={() => (
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition} />
+            <Tasks
+              tasks={tasks}
+              handleTaskClick={handleTaskClick}
+              handleTaskDeletion={handleTaskDeletion}
+            />
+          </>
+        )}/>
+        <Route path="/:taskTitle" exact component={TaskDetails} />
       </div>
-    </>
+    </ Router>
   );
 };
 
