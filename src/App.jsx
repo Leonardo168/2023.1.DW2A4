d857c1c2d5be887928b0524ebf0a2c8332465e72
Leonardo168 <leonardo-96@hotmail.com.br>
2023-06-30 17:22:04 -0300

    apagadas tarefas "default"

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,6 @@ import './App.css'
 
 const App = () => {
   const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      title: 'Estudar DW2A4',
-      completed: false,
-    },
-    {
-      id: '2',
-      title: 'Estudar SESA4',
-      completed: true,
-    },
   ]);
   
   useEffect(() => {
